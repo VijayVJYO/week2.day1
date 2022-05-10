76b783d45d4709a5aa158a1b5f0b04712cdea32a
VijayVJYO <105290970+VijayVJYO@users.noreply.github.com>
2022-05-10 19:02:57 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,32 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+	    driver.get("http://leaftaps.com/opentaps/control/main");
+	    driver.manage().window().maximize();
+	      
+	 driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+	 driver.findElement(By.id("password")).sendKeys("crmsfa");
+	 driver.findElement(By.className("decorativeSubmit")).click();
+	 String text= driver.findElement(By.tagName("h2")).getText();
+	 System.out.println(text);
+	 driver.findElement(By.linkText("CRM/SFA")).click();
+	 String title = driver.getTitle();
+	 System.out.println(title);
+	 driver.findElement(By.linkText("Leads")).click();
+	 driver.findElement(By.linkText("Create Lead")).click();
+	 driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+	 driver.findElement(By.id("createLeadForm_firstName")).sendKeys("VJ");
+	 driver.findElement(By.id("createLeadForm_lastName")).sendKeys("S D");
+	 driver.findElement(By.name("submitButton")).click();
+	 driver.close();
+
+}
+}
